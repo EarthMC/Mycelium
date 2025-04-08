92633192263f25b38cf07f7ab700fa0b1e2c2b0a
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-04-08 06:14:22 +0200

    Document provider

--- api/src/main/java/net/earthmc/mycelium/api/Mycelium.java
+++ api/src/main/java/net/earthmc/mycelium/api/Mycelium.java
@@ -4,6 +4,10 @@ import net.earthmc.mycelium.api.messaging.MessagingRegistrar;
 import net.earthmc.mycelium.api.network.Network;
 
 public interface Mycelium {
+    /**
+     * Static getter for the mycelium api.
+     * @return The mycelium api instance.
+     */
     static Mycelium get() {
         return MyceliumProvider.get();
     }
--- api/src/main/java/net/earthmc/mycelium/api/MyceliumProvider.java
+++ api/src/main/java/net/earthmc/mycelium/api/MyceliumProvider.java
@@ -1,10 +1,21 @@
 package net.earthmc.mycelium.api;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.Nullable;
 
+/**
+ * A static provider for the Mycelium API.
+ */
 public class MyceliumProvider {
     private static @Nullable Mycelium INSTANCE = null;
 
+    private MyceliumProvider() {}
+
+    /**
+     * Only to be used internally.
+     * @param instance The mycelium api instance.
+     */
+    @ApiStatus.Internal
     public static void register(Mycelium instance) {
         if (INSTANCE != null) {
             throw new IllegalStateException("Cannot register mycelium api provider when a provider is already set.");
@@ -13,6 +24,10 @@ public class MyceliumProvider {
         INSTANCE = instance;
     }
 
+    /**
+     * Static getter for the mycelium api.
+     * @return The mycelium api instance.
+     */
     public static Mycelium get() {
         if (INSTANCE == null) {
             throw new IllegalStateException("Mycelium api provider has not been registered yet!");
