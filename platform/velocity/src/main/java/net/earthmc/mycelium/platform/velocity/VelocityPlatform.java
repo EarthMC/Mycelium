a89cbd8c03723e12e6daa5b895816d773c6a5a5a
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-04-10 15:43:20 +0200

    further work on proxy impl

--- platform/velocity/src/main/java/net/earthmc/mycelium/platform/velocity/VelocityPlatform.java
+++ platform/velocity/src/main/java/net/earthmc/mycelium/platform/velocity/VelocityPlatform.java
@@ -3,7 +3,8 @@ package net.earthmc.mycelium.platform.velocity;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
-import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
@@ -17,7 +18,12 @@ import net.earthmc.mycelium.api.proto.ConsoleCommand;
 import net.earthmc.mycelium.client.MyceliumClient;
 import net.earthmc.mycelium.client.impl.proto.PlayerCommandRequest;
 import net.earthmc.mycelium.client.redis.RedisKey;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.slf4j.Logger;
+
+import java.util.Locale;
+import java.util.UUID;
 
 @Plugin(name = "Mycelium", id = "mycelium", version = "0.0.1", authors = "Warriorrr")
 public class VelocityPlatform extends Platform {
@@ -27,7 +33,7 @@ public class VelocityPlatform extends Platform {
     @Inject
     private Logger logger;
 
-    private MyceliumClient client = MyceliumClient.forPlatform(this).build();
+    private final MyceliumClient client = MyceliumClient.forPlatform(this).autoregister().build();
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
@@ -71,13 +77,50 @@ public class VelocityPlatform extends Platform {
     }
 
     @Subscribe
-    public void onPlayerJoin(LoginEvent event) {
+    public void onPreLogin(PreLoginEvent event) {
+        final UUID uuid = event.getUniqueId();
+        final String username = event.getUsername().toLowerCase(Locale.ROOT);
 
+        try (StatefulRedisConnection<String, String> connection = client.client().connect()) {
+            boolean alreadyLoggedIn = false;
+
+            if (uuid != null) {
+                alreadyLoggedIn = connection.sync().sismember(RedisKey.create(client.network().id(), "players"), uuid.toString());
+            } else {
+                alreadyLoggedIn = connection.sync().exists(RedisKey.create(client.network().id(), "name2uuid", username)) == 1;
+            }
+
+            if (alreadyLoggedIn) {
+                event.setResult(PreLoginEvent.PreLoginComponentResult.denied(Component.text("You are already connected to this network.", NamedTextColor.RED)));
+                return;
+            }
+        }
+    }
+
+    @Subscribe
+    public void onPlayerJoin(PostLoginEvent event) {
+        final String uuid = event.getPlayer().getUniqueId().toString();
+        final String username = event.getPlayer().getUsername().toLowerCase(Locale.ROOT);
+
+        try (StatefulRedisConnection<String, String> connection = client.client().connect()) {
+            // todo: check if player doesn't exist to prevent any race conditions, then enter data
+        }
     }
 
     @Subscribe
     public void onPlayerQuit(DisconnectEvent event) {
+        final Player player = event.getPlayer();
 
+        // TODO: Ensure that this is not ran when we deny a player from joining due to already being connected.
+        try (StatefulRedisConnection<String, String> connection = client.client().connect()) {
+            connection.sync().del(RedisKey.create(client.network().id(), "name2uuid", player.getUsername().toLowerCase(Locale.ROOT)));
+
+            // TODO: nicer way to handle keys?
+            final String uuid = player.getUniqueId().toString();
+            connection.sync().srem(RedisKey.create(client.network().id(), "proxy", this.id(), "players"), uuid);
+            connection.sync().srem(RedisKey.create(client.network().id(), "players"), uuid);
+            connection.sync().del(RedisKey.create(client.network().id(), "player", uuid));
+        }
     }
 
     @Override
