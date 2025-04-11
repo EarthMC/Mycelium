57dc63df9f3f9b22954d5fee23affeaf280d3c8e
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-04-11 20:08:29 +0200

    Add api publishing

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 repositories {
@@ -25,5 +26,24 @@ tasks {
         options.links(
             "https://www.javadoc.io/doc/com.google.code.gson/gson/${libs.gson.get().version}/"
         )
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            val releasesUrl = "https://repo.earthmc.net/releases"
+            val snapshotsUrl = "https://repo.earthmc.net/snapshots"
+            url = uri(if (project.version.toString().endsWith("-SNAPSHOT")) snapshotsUrl else releasesUrl)
+
+            name = "earthmc"
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        create<MavenPublication>("library") {
+            from(components.getByName("java"))
+        }
     }
 }
