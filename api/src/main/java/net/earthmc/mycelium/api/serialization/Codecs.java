0cc88baff26b368a931b4cd5d02983cf194e0e1b
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-02-09 13:43:30 +0100

    Add codec fields for all primitives

--- api/src/main/java/net/earthmc/mycelium/api/serialization/Codecs.java
+++ api/src/main/java/net/earthmc/mycelium/api/serialization/Codecs.java
@@ -1,7 +1,7 @@
 package net.earthmc.mycelium.api.serialization;
 
 /**
- * A class holding simple codecs for common and otherwise primitive types.
+ * A class holding simple codecs for common and primitive types.
  */
 public final class Codecs {
     private Codecs() {}
@@ -12,7 +12,37 @@ public final class Codecs {
     public static final JsonCodec<String> STRING = JsonCodec.simple(String.class);
 
     /**
-     * A standard {@link Integer} codec
+     * A standard {@link Integer} codec.
      */
     public static final JsonCodec<Integer> INTEGER = JsonCodec.simple(Integer.class);
+
+    /**
+     * A standard {@link Long} codec.
+     */
+    public static final JsonCodec<Long> LONG = JsonCodec.simple(Long.class);
+
+    /**
+     * A standard {@link Boolean} codec.
+     */
+    public static final JsonCodec<Boolean> BOOLEAN = JsonCodec.simple(Boolean.class);
+
+    /**
+     * A standard {@link Short} codec.
+     */
+    public static final JsonCodec<Short> SHORT = JsonCodec.simple(Short.class);
+
+    /**
+     * A standard {@link Double} codec.
+     */
+    public static final JsonCodec<Double> DOUBLE = JsonCodec.simple(Double.class);
+
+    /**
+     * A standard {@link Float} codec.
+     */
+    public static final JsonCodec<Float> FLOAT = JsonCodec.simple(Float.class);
+
+    /**
+     * A standard {@link Character} codec.
+     */
+    public static final JsonCodec<Character> CHARACTER = JsonCodec.simple(Character.class);
 }
