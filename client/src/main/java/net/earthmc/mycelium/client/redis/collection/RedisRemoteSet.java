4fbd9c0c11cdf79f21731a63513a3d5fed73fa2e
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-04-28 18:40:36 +0200

    Remove closing behavior of RedisRemoteSet
    No underlying connection exists anymore, so there's no reason to have it be closable anymore

--- client/src/main/java/net/earthmc/mycelium/client/redis/collection/RedisRemoteSet.java
+++ client/src/main/java/net/earthmc/mycelium/client/redis/collection/RedisRemoteSet.java
@@ -5,7 +5,6 @@ import net.earthmc.mycelium.client.MyceliumClient;
 import net.earthmc.mycelium.client.impl.serialization.RedisCodec;
 import org.jspecify.annotations.NullMarked;
 
-import java.io.Closeable;
 import java.util.AbstractSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -13,13 +12,10 @@ import java.util.stream.Collectors;
 
 /**
  * A wrapper around a redis set that converts the remote data to a usable class using the supplied codec. The remote set will be (de)serialized on each access, guaranteeing that the returned data is up-to-date.
- * <p>
- * After calling {@link #close()}, the underlying connection will be closed and this set will act like an empty set.
  */
 @NullMarked
-public class RedisRemoteSet<T> extends AbstractSet<T> implements Closeable {
-    protected boolean closed;
-    protected final String redisKey;
+public class RedisRemoteSet<T> extends AbstractSet<T> {
+    private final String redisKey;
     private final MyceliumClient client;
     private final RedisCodec<T> codec;
 
@@ -31,39 +27,23 @@ public class RedisRemoteSet<T> extends AbstractSet<T> implements Closeable {
     }
 
     public Set<T> getRemote() {
-        if (this.closed) {
-            return Set.of();
-        }
-
         return this.client.redis().smembers(this.redisKey).stream().map(codec::deserialize).collect(Collectors.toSet());
     }
 
     @Override
     public boolean add(T element) {
-        if (this.closed) {
-            return false;
-        }
-
         return this.client.redis().sadd(this.redisKey, this.codec.serialize(element)) == 1;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public boolean remove(Object element) {
-        if (this.closed) {
-            return false;
-        }
-
         return this.client.redis().srem(this.redisKey, this.codec.serialize((T) element)) == 1;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public boolean contains(Object element) {
-        if (this.closed) {
-            return false;
-        }
-
         return this.client.redis().sismember(this.redisKey, this.codec.serialize((T) element));
     }
 
@@ -74,20 +54,19 @@ public class RedisRemoteSet<T> extends AbstractSet<T> implements Closeable {
 
     @Override
     public int size() {
-        if (this.closed) {
-            return 0;
-        }
-
         return this.client.redis().smembers(this.redisKey).size();
     }
 
     @Override
-    public void close() {
-        this.closed = true;
+    public void clear() {
+        this.client.redis().del(this.redisKey);
     }
 
     @Override
     public String toString() {
-        return this.getRemote().toString();
+        return "RedisRemoteSet{" +
+                "redisKey='" + redisKey + '\'' +
+                ", remote=" + this.getRemote() +
+                '}';
     }
 }
